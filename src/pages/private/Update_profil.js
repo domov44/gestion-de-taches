49e9f7f286d0e07c1c211008f202b3b28d40acdd
Eddie klaï <91609481+Eddieklai@users.noreply.github.com>
2024-02-06 16:29:34 +0100

    [ADD] Update user name and cellphone successfully

--- src/pages/private/Update_profil.js
+++ src/pages/private/Update_profil.js
@@ -1,28 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useUser} from "../../hooks/UserContext";
+import {updateUserAttributes} from 'aws-amplify/auth';
+
 
 export default function Update_profil() {
     const {user} = useUser();
-    
+    const [email, setEmail] = useState(user.email);
+    const [name, setName] = useState(user.name);
+    const [phone, setPhoneNumber] = useState(user.phone_number);
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePhoneChange = (event) => {
+        setPhoneNumber(event.target.value);
+    };
+
+    const handleUpdateEmailAndNameAttributes = (event) => {
+        event.preventDefault();
+        updateUserAttributes({
+            userAttributes: {
+                email: email,
+                name: name,
+                phone_number: phone,
+            },
+        })
+          .then((result) => {
+              console.log(result);
+          })
+          .catch((error) => {
+              console.log(error);
+          });
+    }
+
     return (
       <div className="app__Update_profil">
-            <h1>Update_profil</h1>
-          <form>
-                <div>
-                    <label htmlFor="name">Nom</label>
-                    <input type="text" id="name" name="name" value={user.name}/>
-                </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" value={user.email}/>
-                </div>
-                <div>
-                    <label htmlFor="phone">Téléphone</label>
-                    <input type="tel" id="phone" name="phone" value={user.phone_number}/>
-                </div>
-                <div>
-                    <button type="submit">Mettre à jour</button>
-                </div>
+          <h1>Update_profil {user.name}</h1>
+          <form onSubmit={handleUpdateEmailAndNameAttributes}>
+              <div>
+                  <label htmlFor="name">Nom</label>
+                  <input type="text" id="name" name="name" value={name} onChange={handleNameChange}/>
+              </div>
+              <div>
+                  <label htmlFor="email">Email</label>
+                  <input type="email" id="email" name="email" value={email} onChange={handleEmailChange}/>
+              </div>
+              <div>
+                  <label htmlFor="phone">Téléphone</label>
+                  <input type="tel" id="phone" name="phone" value={phone} onChange={handlePhoneChange}/>
+              </div>
+              <div>
+                  <button type="submit">Mettre à jour</button>
+              </div>
           </form>
       </div>
     )
--- src/pages/public/Connect.js
+++ src/pages/public/Connect.js
@@ -2,14 +2,14 @@
 import React from 'react';
 import { useUser } from '../../hooks/UserContext';
 import { Amplify } from 'aws-amplify';
-import { Auth } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from '../../aws-exports';
 import CustomSignIn from '../../components/CustomSigIn';
 import CustomSignOut from '../../components/CustomSignOut';
 Amplify.configure(awsExports);
+
+const myApi = "GestionTachesAPI";
 
 export default function Connect() {
     const { login, logout, isLoggedIn } = useUser();
@@ -19,6 +19,7 @@ export default function Connect() {
           {isLoggedIn ? (
             <>
                 <CustomSignOut />
+
             </>
           ) : (
             <>
